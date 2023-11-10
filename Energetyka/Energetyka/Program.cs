45de0b54f5f84d787bba68f8faed688ea56345b6
DWprog <dawid.window@gmail.com>
2023-11-10 13:39:52 +0100

    Initial commit

---
+++ Energetyka/Energetyka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Energetyka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
