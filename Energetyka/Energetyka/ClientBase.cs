f3b6b6078704922d6c42c32234a7b3814dc35015
DWprog <dawid.window@gmail.com>
2023-11-10 15:19:16 +0100

    Przygotowanie interface i klasy bazowej

---
+++ Energetyka/Energetyka/ClientBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Energetyka
+{
+    public abstract class ClientBase : IClient
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public float Declaration { get; set; }
+        public float Limit { get; set; }
+
+        public ClientBase(string name,string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public ClientBase(string name,string surname,float declaration=0,float limit=0)
+        {
+            Name = name;
+            Surname = surname;
+            Declaration = declaration;
+            Limit = limit;
+        }
+
+        public abstract void AddUnits(float units);
+        public abstract void AddUnits(double units);
+        public abstract void AddUnits(int units);
+        public abstract void AddUnits(string units);
+    }
+}
---
+++ Energetyka/Energetyka/IClient.cs
@@ -0,0 +1,17 @@
+﻿using static Energetyka.ClientBase;
+
+namespace Energetyka
+{
+    public interface IClient
+    {
+        string Name { get; }
+        string Surname { get; }
+        float Declaration { get; set; }
+        float Limit { get; set; }
+
+        void AddUnits(float units);
+        void AddUnits(double units);
+        void AddUnits(int units);
+        void AddUnits(string units);
+    }
+}
